d32720a4771ff157799aba5af156b5d23d4c6e2b
EmmyMay <lekeemmy@gmail.com>
2020-08-08 03:12:47 +0100

    removed configureWebpack Object

--- vue.config.js
+++ vue.config.js
@@ -11,21 +11,84 @@ module.exports = {
         args[0].title = 'PinFlix'
         return args
       })
-  },
-  configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        indexPath: path.resolve('public/index.html'),
-        staticDir: path.resolve('dist'),
-        routes: ['/', '/maintwi', '/maintik', '/admin', '/upload'],
-        renderer: new PrerenderSPAPlugin.PuppeteerRenderer({
-          injectProperty: '__prerender',
-          inject: {},
-          renderAfterDocumentEvent: 'render-ready',
-        })
-      }),
-
-    ],
-
   }
+}
+
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [{
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: false,
+  },
+  devtool: '#eval-source-map',
+  plugins: [
+    new VueLoaderPlugin(),
+  ]
+}
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+
+    new PrerenderSPAPlugin({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/', '/maintwi', '/maintik'],
+
+      renderer: new Renderer({
+        inject: {
+          foo: 'bar'
+        },
+        headless: true,
+        renderAfterDocumentEvent: 'render-event'
+      })
+    })
+  ])
 }
